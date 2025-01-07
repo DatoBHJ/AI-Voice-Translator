bb50a4e2e6e85db78c7b65f0e4f88714b8f45b4d
datobhj <bdfutbol@naver.com>
2025-01-07 12:41:59 +0900

    deactivating eslintrc

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ESLint 에러가 있어도 빌드가 진행되도록 설정
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

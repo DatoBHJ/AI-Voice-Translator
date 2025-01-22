f919b78b0dbadda22ba287b59af9db3b570407b2
datobhj <bdfutbol@naver.com>
2025-01-22 19:23:43 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -247,13 +247,15 @@ export default function Home() {
         )}
 
         {error && (
-          <div className="fixed top-20 left-0 right-0 mx-auto px-4 z-[70] max-w-md">
-            <div className="mt-4 p-4 bg-red-50 text-red-700 rounded-lg border border-red-200 shadow-lg animate-slideDown">
-              <div className="flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-                <span className="text-sm">{error}</span>
+          <div className="fixed top-20 inset-x-0 z-[70] flex justify-center">
+            <div className="w-[calc(100%-2rem)] max-w-md">
+              <div className="p-4 bg-red-50 text-red-700 rounded-lg border border-red-200 shadow-lg animate-slideDown">
+                <div className="flex items-center gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  </svg>
+                  <span className="text-sm leading-5">{error}</span>
+                </div>
               </div>
             </div>
           </div>
